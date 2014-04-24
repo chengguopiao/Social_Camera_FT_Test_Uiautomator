5f977120368b00db916c5de6b13943ab43373ebc
chengguopiao <chengguo.piao@borqs.com>
2014-04-24 16:14:35 +0800

    Update continuous.py

--- script/continuous.py
+++ script/continuous.py
@@ -337,7 +337,7 @@ class CameraTest(unittest.TestCase):
         """ 
         # step 2
         sm.setCameraSetting('single',3,1)
-        assert bool(a.cmd('cat',PATH + LOCATION_KEY).find('on')+1)
+        assert bool(a.cmd('cat',PATH1 + LOCATION_KEY).find('on')+1)
         # step 3
         self._ContinuouCapturePic()          
 
@@ -352,7 +352,7 @@ class CameraTest(unittest.TestCase):
         """
         # step 2
         sm.setCameraSetting('single',3,2)
-        assert bool(a.cmd('cat',PATH + LOCATION_KEY).find('off')+1)
+        assert bool(a.cmd('cat',PATH1 + LOCATION_KEY).find('off')+1)
         # step 3
         self._ContinuouCapturePic()   
 
