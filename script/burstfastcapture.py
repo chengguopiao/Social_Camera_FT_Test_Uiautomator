3e20357505bf7ab6e5468e6f984c19fb097b87eb
chengguopiao <chengguo.piao@borqs.com>
2014-06-06 17:47:22 +0800

    Update burstfastcapture.py

--- script/burstfastcapture.py
+++ script/burstfastcapture.py
@@ -184,7 +184,7 @@ class CameraTest(unittest.TestCase):
         '''
         SM.setCameraSetting('burst',3,5)
         assert bool(AD.cmd('cat',SCENE_STATE).find('night')+1)
-        self._captureAndCheckPicCount('single',5)
+        self._captureAndCheckPicCount('single',10)
 
     def testCapturePictureWithScenesLandscape(self):
         '''
