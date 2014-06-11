a49f3f3d32713089e91270394951441fc561f02f
chengguopiao <chengguo.piao@borqs.com>
2014-06-11 15:14:25 +0800

    Update quickswitch.py

--- script/quickswitch.py
+++ script/quickswitch.py
@@ -33,7 +33,7 @@ class CameraTest(unittest.TestCase):
         #Step 1. Launch single capture activity
         a.cmd('launch','com.intel.camera22/.Camera')
         time.sleep(2)
-        if  d(text = 'OK').wait.exists(timeout = 3000):
+        if d(text = 'OK').wait.exists(timeout = 3000):
             d(text = 'OK').click.wait()
         assert d(resourceId = 'com.intel.camera22:id/shutter_button'),'Launch camera failed!!'
 
